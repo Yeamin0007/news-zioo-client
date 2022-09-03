e74c7946036d28f88f1b4f824067850092bb8c97
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-09-03 18:47:11 +0600

    commit complete modal

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const loadCategories = () =>{
+
+const loadAllNews = () =>{
     const url ='https://openapi.programming-hero.com/api/news/categories';
 
     fetch (url)
@@ -10,14 +11,16 @@ const loadCategories = () =>{
     const ulContainer = document.getElementById('ul-catagory');
 
     news_category.forEach(category =>{
-        console.log(category)
+         console.log(category)
         const categoryList = document.createElement('li');
         
         categoryList.innerHTML = `
-        <li onclick= "showNews('${category.category_id}')" class="list-group-item"> <a class="text-secondary text-decoration-none fs-5" href="#">${category.category_name}</a> </li>
-        `;
+        <li onclick= "showNews('${category.category_id}')" class="list-group-item"> <a class="text-secondary text-decoration-none fs-5" href="#">${category.category_name}</a> </li>`;
+       
         ulContainer.appendChild(categoryList);
     })
+
+   
 
 }
 
@@ -27,12 +30,32 @@ const showNews =(News) =>{
     .then (res => res.json())
     .then (data => displayNews(data.data));
 }
+const toggleSpinner = isLoading => {
+  const loadingSection = document.getElementById('loader');
+  if(isLoading){
+    loadingSection.classList.remove('d-none')
+  }
+  else{
+    loadingSection.classList.add('d-none');
+  }
+}
 
 const displayNews = news => {
+  toggleSpinner(true);
+  if(news.length == 0){
+    toggleSpinner(false); 
+    const error = document.getElementById('error');
+    error.innerText = " Data not Found."
+    
+    
+  } 
+  else{
+
+
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = ``;
     news.forEach(categoryNews =>{
-        console.log(categoryNews)
+         console.log(categoryNews)
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('row')
         
@@ -44,7 +67,7 @@ const displayNews = news => {
                   <div class="col-md-8">
                     <div class="card-body">
                       <h5 class="card-title">${categoryNews.title}</h5>
-                      <p class="card-text">${categoryNews.details.slice(0, 200)}</p>
+                      <p class="card-text">${categoryNews.details.slice(0, 200)}...</p>
                     
                       <div class = "d-flex justify-content-between pt-5">
                       <div class= "d-flex ">
@@ -58,10 +81,10 @@ const displayNews = news => {
                     <div class= "d-flex">
                     <i class="bi bi-eye-fill px-2"></i>
 
-                    <p class="card-text"><small class="text-muted">${categoryNews.total_view}</small></p>
+                    <p class="card-text"><small class="text-muted">${categoryNews.total_view ? categoryNews.total_view : 'no view yet'}</small></p>
 
                     </div>
-                    <button onclick class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><small>Read more..</small></button>
+                    <button onclick="loadNewsDetails('${categoryNews._id}')" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><small>Read more..</small></button>
                     
                       </div>
                     
@@ -70,9 +93,40 @@ const displayNews = news => {
             
         `;
         newsContainer.appendChild(newsDiv);
-    })
+        
+        toggleSpinner(false);
+
+   })
+  }
+   
+  error.innerText='';
+       
 
 }
 
 
-loadCategories();
+
+const loadNewsDetails = (_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${_id}`;
+  fetch (url)
+  .then (res => res.json())
+  .then (data => modalNewsDetails(data.data));
+}
+
+const modalNewsDetails = modalDetails =>{
+  console.log(modalDetails)
+  const modalTitle= document.getElementById('newsDetailModalLabel');
+  modalTitle.innerText = modalDetails[0].title;
+  const modalBody = document.getElementById('modal-news');
+  modalBody.innerHTML=`
+  <img class="my-4 w-100" src="${modalDetails[0].thumbnail_url}"></img>
+  <p>"${modalDetails[0].details.slice(0, 100)}"</p>
+  <p>Author Name: "${modalDetails[0].author.name ? modalDetails[0].author.name : 'Not Found'}"<p>
+  <p>Published Date: "${modalDetails[0].author.published_date ? modalDetails[0].author.published_date : 'Not Found'}"</p>
+  `
+}
+
+// loadNewsDetails();
+
+
+loadAllNews();
